184b977270cbad18cc6108905064e6866a397841
Elton133 <134627073+Elton133@users.noreply.github.com>
2024-08-28 15:11:03 +0000

    Movies

--- app.js
+++ app.js
@@ -12,34 +12,6 @@ form.addEventListener("submit", async function (e) {
   form.elements.query.value = "";
 });
 
-// const displayImages = (shows) => {
-//   for (let result of shows) {
-//     if (result.show.image) {
-//       const img = document.createElement("IMG");
-//       img.src = result.show.image.medium;
-//       document.body.append(img);
-//     }
-
-//     const title = document.createElement("h3");
-//     title.innerText = result.show.name;
-//     document.body.append(title);
-
-//     const rating = document.createElement("p");
-//     rating.innerText = `Rating: ${result.show.rating.average || "N/A"}`;
-//     document.body.append(rating);
-
-//     const country = document.createElement("p");
-//     country.innerText = `Country: ${
-//       result.show.network ? result.show.network.country.name : "N/A"
-//     }`;
-//     document.body.append(country);
-
-//     const genre = document.createElement("p");
-//     genre.innerText = `Genre: ${
-//       result.show.genres.length > 0 ? result.show.genres.join(", ") : "N/A"
-//     }`;
-//     document.body.append(genre);
-//   }
 const displayImages = (shows) => {
   // Clear previous results
   document.body.innerHTML = "";
